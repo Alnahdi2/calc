f07d74c3c8d0940171fccfa7d14d7384a08b82a1
Alnahdi2 <atn11111@gmail.com>
2025-01-14 14:36:51 +0300

    clean repo

--- Modules/root.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def calculate_square_root(number):
-    if number < 0:
-        raise ValueError("Cannot calculate the square root of a negative number.")
-    return math.sqrt(number)
